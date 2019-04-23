3cd834dc8fdf02ea663ce5f085ec8cb9acc626eb
Ha Quoc Tien <17521122@gm.uit.edu.vn>
2019-04-23 20:44:37 +0700

    Create NE.py

---
+++ NE.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 20:30:38 2019
+
+@author: tien
+"""
+from BGD import add1Col
+import numpy as np
+
+def NormalEquation(x, y):
+    x = add1Col(x)
+    y = y.flatten()
+    w = np.linalg.inv((x.transpose().dot(x))).dot(x.transpose().dot(y))    
+    return w
